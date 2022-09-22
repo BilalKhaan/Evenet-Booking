87f13bc1b166995438a0cb785b2936b89c67afe8
Muhammad Bilal Anjum <bilalanjum@muhammads-mbp.home>
2022-09-22 18:34:41 +0500

    path to file has been updated

--- src/store.js
+++ src/store.js
@@ -24,10 +24,7 @@ export default createStore({
 
   actions: {
     async fetchEvents (context) {
-      console.log(process, 'mode')
-      const baseUrl = process.env.NODE_ENV == 'production' ? '' : 'http://localhost'
-      const port = process.env.NODE_ENV == 'production' ? 5000 : 8080
-      await axios.get(`${baseUrl}:${port}/events.json`).then(response => { 
+      await axios.get(`/events.json`).then(response => { 
         let data = EventMaper.mapEvents(response.data)
         context.commit('saveEvents', data)
       })
