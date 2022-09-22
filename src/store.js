42b90679e6fcc09ee05f111994086e9ffc939779
Muhammad Bilal Anjum <bilalanjum@muhammads-mbp.home>
2022-09-22 18:19:17 +0500

    production url is removed

--- src/store.js
+++ src/store.js
@@ -24,8 +24,8 @@ export default createStore({
 
   actions: {
     async fetchEvents (context) {
-      console.log(process.env.NODE_ENV, 'mode')
-      const baseUrl = process.env.NODE_ENV == 'production' ? 'https://evenet-booking.herokuapp.com' : 'http://localhost'
+      console.log(process, 'mode')
+      const baseUrl = process.env.NODE_ENV == 'production' ? '' : 'http://localhost'
       const port = process.env.NODE_ENV == 'production' ? 5000 : 8080
       await axios.get(`${baseUrl}:${port}/events.json`).then(response => { 
         let data = EventMaper.mapEvents(response.data)
